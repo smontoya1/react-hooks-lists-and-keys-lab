5fd283d466967a805d55c4aa511a89bcc013a346
Shaun <smontoya0009@gmail.com>
2025-10-26 15:07:35 -0400

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,8 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        <span className="projectItem">{technologies[0]}</span>
+        <span className="projectItem">{technologies[1]}</span>
       </div>
     </div>
   );
